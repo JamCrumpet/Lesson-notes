a822ac7d7696fa8c05dab2f023b8367a5b4122f4
JamCrumpet <62818861+JamCrumpet@users.noreply.github.com>
2020-08-19 17:23:57 +0100

    Add files via upload

---
+++ string_syntax_errors.py
@@ -0,0 +1,4 @@
+message ="One of Python's many strengths is its diverse commnunity." # This runs fine as apostrophe appears whithin double quotes
+print(message)
+
+message_2 = 'One of Python's many strenghts is its diverse community' # This chauses an error as it contains a single quote and python cannot identify where the string should end
